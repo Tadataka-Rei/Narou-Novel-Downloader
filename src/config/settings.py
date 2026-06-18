b366240c271910526082bc43f6ea018184b46446
Tevel <thuathan64@gmail.com>
2026-06-18 12:58:57 +0700

    chore: fixing stuff

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,22 +5,14 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class DownloaderSettings:
-    # Sleep between chapter downloads when doing a full novel scan
     sleep_seconds: float = 5.0
 
-    # Maximum chapters to scan/download when using a novel URL.
-    # 0 means "no cap".
+
     max_chapters_scan: int = 0
 
-    # Ruby/tag replacement behavior for furigana.
-    # When enabled, ruby elements will be converted as:
-    #   replacement_format.format(base=..., furigana=...)
     enable_ruby_replacement: bool = True
     replacement_format: str = "{base}[{furigana}]"
 
-    # When downloading a novel (index page), start from this chapter number.
-    # 1 means "start from the beginning".
-    # Note: For direct chapter URLs, this setting is ignored.
     start_from_chapter: int = 1
 
 
